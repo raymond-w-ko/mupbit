958d77a0b6a6724b78e4eded021edd60b6300234
Raymond W. Ko <raymond.w.ko@gmail.com>
2013-11-20 22:43:15 -0500

    implement add, sub, neg, cmp for BigInt

--- BigInt.cpp
+++ BigInt.cpp
@@ -6,7 +6,6 @@ BigInt::~BigInt() {
 
 BigInt::BigInt()
     : sign(1) {
-  digits.push_back(0);
 }
 
 BigInt::BigInt(unsigned int n) 
@@ -45,25 +44,179 @@ void BigInt::swap(BigInt& other) {
   std::swap(this->digits, other.digits);
 }
 
-BigInt operator+(const BigInt& lhs, const BigInt& rhs) {
-  std::vector<uint32_t> bigger =
+std::string BigInt::str(int base) {
+  // TODO: implement other bases
+  assert(base == 16);
+
+  std::string s;
+  if (sign < 0)
+    s += "-";
+
+  char buf[9];
+  for (size_t i = 0; i < digits.size(); ++i) {
+    size_t index = digits.size() - i - 1;
+    SmallBaseInt n = digits[index];
+    sprintf(buf, "%08x", n);
+    s += buf;
+  }
+  return s;
+}
+
+BigInt BigInt::operator-() const {
+  BigInt n(*this);
+  n.sign > 0 ? -1 : 1;
+  return n;
+}
+
+BigInt BigInt::operator+(const BigInt& rhs) const {
+  const BigInt& lhs = *this;
+
+  if (lhs.sign > 0 && rhs.sign < 0) {
+    // (+123) + (-345) == (+123) - (+345)
+    return lhs.operator-(-rhs);
+  } else if (lhs.sign < 0 && rhs.sign > 0) {
+    // (-123) + (+345) == (+345) - (+123)
+    return rhs.operator-(-lhs);
+  }
+
+  /*
+  if (lhs.sign > 0 && rhs.sign > 0) {
+    // (+123) + (+345) == 123 + 345
+  }
+  else if (lhs.sign < 0 && rhs.sign < 0) {
+    // (-123) + (-345) == -(123 + 345)
+  }
+  */
+  assert((lhs.sign > 0 && rhs.sign > 0) || (lhs.sign < 0 && rhs.sign < 0));
+
+  BigInt sum;
+
+  const std::vector<SmallBaseInt>& bigger =
       lhs.digits.size() >= rhs.digits.size() ? lhs.digits : rhs.digits;
-  const std::vector<uint32_t>& smaller =
+  const std::vector<SmallBaseInt>& smaller =
       lhs.digits.size() < rhs.digits.size() ? lhs.digits : rhs.digits;
 
-  for (int i = 0; i < smaller.size(); ++i) {
-    uint32_t digitSum = bigger[i] + smaller[i];
-    uint32_t digitOverflow = digitSum < bigger[i] ? 1 : 0;
+  BigBaseInt carry = 0;
+  size_t i;
+  for (i = 0; i < smaller.size(); ++i) {
+    BigBaseInt wideDigitSum =
+        static_cast<BigBaseInt>(carry) +
+        static_cast<BigBaseInt>(bigger[i]) +
+        static_cast<BigBaseInt>(smaller[i]);
+    SmallBaseInt digitSum = static_cast<SmallBaseInt>(wideDigitSum);
+    sum.digits.push_back(digitSum);
+    carry = wideDigitSum >> sizeof(SmallBaseInt) * 8;
+  }
+  for (; i < bigger.size(); ++i) {
+    BigBaseInt wideDigitSum =
+        static_cast<BigBaseInt>(carry) +
+        static_cast<BigBaseInt>(bigger[i]);
+    SmallBaseInt digitSum = static_cast<SmallBaseInt>(wideDigitSum);
+    sum.digits.push_back(digitSum);
+    carry = wideDigitSum >> sizeof(SmallBaseInt) * 8;
+  }
+  if (carry > 0) {
+    sum.digits.push_back(static_cast<SmallBaseInt>(carry));
+  }
+  sum.sign = lhs.sign;
+  return sum;
+}
 
-    if (digitOverflow > 0) {
-      const int nextIndex = i + 1;
-      if (nextIndex >= bigger.size()) {
-        bigger.push_back(digitOverflow);
-      } else {
-        // XXX: doesn't work , can cause overflow!
-        bigger[nextIndex] += digitOverflow;
-      }
+BigInt BigInt::operator-(const BigInt& rhs) const {
+  const BigInt& lhs = *this;
+
+  if (lhs.sign > 0 && rhs.sign < 0) {
+    // (+123) - (-345) == (+123) + (+345)
+    return lhs.operator+(-rhs);
+  } else if (lhs.sign < 0 && rhs.sign > 0) {
+    // (-123) - (+345) == -((+345) + (+123))
+    BigInt tmp = rhs.operator+(-lhs);
+    tmp.sign = -1;
+    return tmp;
+  }
+
+  char operandSignFlip;
+  if (lhs.sign > 0 && rhs.sign > 0) {
+    // (+123) - (+345) == 123 - 345
+    operandSignFlip = 1;
+  }
+  else /*if (lhs.sign < 0 && rhs.sign < 0)*/ {
+    // (-123) - (-345) == (+345) - 123
+    operandSignFlip = -1;
+  }
+  assert(lhs.sign > 0 && rhs.sign > 0);
+
+  BigInt diff;
+
+  bool cmp = lhs <= rhs;
+
+  const std::vector<SmallBaseInt>& bigger =
+      cmp ? rhs.digits : lhs.digits;
+  const std::vector<SmallBaseInt>& smaller =
+      cmp ? lhs.digits : rhs.digits;
+
+  char magnitudeFlip = cmp ? -1 : 1;
+
+  size_t borrow = 0;
+  for (size_t i = 0; i < bigger.size(); ++i) {
+    BigBaseInt wideDigitDiff =
+        static_cast<BigBaseInt>(bigger[i]) -
+        static_cast<BigBaseInt>(smaller[i]) -
+        static_cast<BigBaseInt>(borrow);
+    SmallBaseInt digitDiff = static_cast<SmallBaseInt>(wideDigitDiff);
+    diff.digits.push_back(digitDiff);
+    borrow = ((borrow >> 32) > 0 ? 1 : 0);
+  }
+  assert(borrow == 0);
+
+  diff.sign *= operandSignFlip * magnitudeFlip;
+  return diff;
+}
+
+bool BigInt::operator<(const BigInt& rhs) const {
+  const BigInt& lhs = *this;
+
+  if (lhs.sign > rhs.sign) {
+    return true;
+  } else if (lhs.sign < rhs.sign) {
+    return false;
+  }
+
+  if (lhs.digits.size() < rhs.digits.size()) {
+    return lhs.sign > 0 ? true : false;
+  } else if (lhs.digits.size() > rhs.digits.size()) {
+    return lhs.sign > 0 ? false : true;
+  }
+
+  assert(lhs.sign == rhs.sign);
+  assert(lhs.digits.size() == rhs.digits.size());
+
+  for (size_t i = 0; i < lhs.digits.size(); ++i) {
+    size_t index = lhs.digits.size() - i - 1;
+    SmallBaseInt m = lhs.digits[index];
+    SmallBaseInt n = rhs.digits[index];
+
+    if (m < n) {
+      return lhs.sign > 0 ? true : false;
+    } else if (m > n ) {
+      return lhs.sign > 0 ? false : true;
     }
   }
-  return lhs;
+
+  // they are equal
+  return false;
+}
+
+bool BigInt::operator<=(const BigInt& rhs) const {
+  const BigInt& lhs = *this;
+  return lhs < rhs || lhs == rhs;
+}
+
+bool BigInt::operator>(const BigInt& rhs) const {
+  return !(rhs < *this);
+}
+
+bool BigInt::operator==(const BigInt& rhs) const {
+  const BigInt& lhs = *this;
+  return lhs.sign == rhs.sign && lhs.digits == rhs.digits;
 }
--- BigInt.hpp
+++ BigInt.hpp
@@ -1,11 +1,10 @@
 #pragma once
 
 class BigInt {
-  friend BigInt operator+(const BigInt& lhs, const BigInt& rhs);
- public:
-  // 0 constructor
-  BigInt();
+  typedef uint32_t SmallBaseInt;
+  typedef uint64_t BigBaseInt;
 
+ public:
   BigInt(unsigned int n);
   BigInt(int n);
 
@@ -14,11 +13,21 @@ class BigInt {
   BigInt& operator=(BigInt other);
   BigInt(BigInt&& other);
 
+  BigInt operator+(const BigInt& rhs) const;
+  BigInt operator-(const BigInt& rhs) const;
+  BigInt operator-() const;
+
+  bool operator<(const BigInt& rhs) const;
+  bool operator>(const BigInt& rhs) const;
+  bool operator==(const BigInt& rhs) const;
+  bool operator<=(const BigInt& rhs) const;
+
+  std::string str(int base = 16);
+
  private:
+  BigInt();
   void swap(BigInt& other);
 
   char sign;
-  std::vector<uint32_t> digits;
+  std::vector<SmallBaseInt> digits;
 };
-
-BigInt operator+(const BigInt& lhs, const BigInt& rhs);
